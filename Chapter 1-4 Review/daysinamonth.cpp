120150fd3c4a19d0fe3a9faa18f15641636e7f0d
Miguel Juarez <miguela.juarez456@gmail.com>
2024-03-05 10:38:35 -0800

    changes

--- Chapter 1-4 Review/daysinamonth.cpp
+++ Chapter 1-4 Review/daysinamonth.cpp
@@ -17,18 +17,34 @@ int main() {
     
     //Declare all variables
     int numMnth = 0;    //Number of months
+    int  year = 0;  //Year chosen
+    int numDys = 0; //Number of days
     //Initialize all variables
     
     //Process or Map solution
-    cout<< " Input a month (1-12): ";
+    cout << " Input a month (1-12): ";
     cin >> numMnth;
     if (numMnth > 12 || numMnth <= 0){
         return 0;
     }else{
         cout << "Enter a year: "<< endl;
+        cin >> year;
     }
 
-    if () 
+    switch (year){
+        case 1: numDys = 31;
+        case 2: numDys = 28;
+        case 3: numDys = 31;
+        case 4: numDys = 30;
+        case 5: numDys = 31;
+        case 6: numDys = 30;
+        case 7: numDys = 31;
+        case 8: numDys = 31;
+        case 9: numDys = 30;
+        case 10: numDys = 31;
+        case 11: numDys = 30;
+        case 12: numDys = 31;
+    }
 
     //Display the output
     
