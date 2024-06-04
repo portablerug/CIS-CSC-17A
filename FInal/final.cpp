017220a9b2d9974a4553e959e8341999ca1230fc
miguela.juarez456 <miguela.juarez456@gmail.com>
2024-06-04 04:37:04 +0000

    hopefully pushes

---
+++ FInal/final.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Structures
+struct DominosPayroll {
+
+    //User's address
+    int address;
+
+    //Useres social security number
+    int SSN;
+
+}
+
+int main(){
+    //Define Variables
+    
+    //Initialize variables
+
+    //
+
+    //Exit
+    return 0;
+}
